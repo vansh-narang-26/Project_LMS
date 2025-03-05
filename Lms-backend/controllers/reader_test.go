95c37678e31b61d06a4771b3d044cd170f1a00c8
Vansh Narang <xs522-vannar@xs-host603-055.xs.neuralcompany.team>
2025-03-06 03:02:38 +0530

    Half done added raise req

--- Lms-backend/controllers/reader_test.go
+++ Lms-backend/controllers/reader_test.go
@@ -91,12 +91,75 @@ func TestSearchBooks(t *testing.T) {
 
 // 	router.ServeHTTP(w, req)
 
-// 	assert.Equal(t, http.StatusCreated, w.Code)
-// 	var response map[string]interface{}
-// 	json.Unmarshal(w.Body.Bytes(), &response)
-// 	assert.Contains(t, response, "error")
-// 	assert.Equal(t, "Request created successfully", response["Request created successfully"])
-// }
+//		assert.Equal(t, http.StatusCreated, w.Code)
+//		var response map[string]interface{}
+//		json.Unmarshal(w.Body.Bytes(), &response)
+//		assert.Contains(t, response, "error")
+//		assert.Equal(t, "Request created successfully", response["Request created successfully"])
+//	}
+
+func TestRaiseIssueRequest(t *testing.T) {
+	setupTestDB1()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	IntialiseRoutes(router)
+
+	tests := []struct {
+		name       string
+		query      string
+		headers    map[string]string
+		wantStatus int
+		wantKey    string
+		wantMsg    string
+	}{
+		// {
+		// 	name:       "Request created",
+		// 	query:      "qa",
+		// 	headers:    map[string]string{"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InIyQGdtYWlsLmNvbSIsImlkIjoyMiwicm9sZSI6InJlYWRlciJ9.J3-92_iRT7sAJUBV3fxkHi_4kz3bCxNz6kDPBTSLeJQ"},
+		// 	wantStatus: http.StatusCreated,
+		// 	wantKey:    "Message",
+		// 	wantMsg:    "Request created successfully",
+		// },
+		{
+			name:       "Already requested",
+			query:      "q",
+			headers:    map[string]string{"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InIyQGdtYWlsLmNvbSIsImlkIjoyMiwicm9sZSI6InJlYWRlciJ9.J3-92_iRT7sAJUBV3fxkHi_4kz3bCxNz6kDPBTSLeJQ"},
+			wantStatus: http.StatusBadRequest,
+			wantKey:    "Message",
+			wantMsg:    "The book has been already requested by you",
+		},
+		{
+			name:       "Request created",
+			query:      "aa", //book which is not avialable
+			headers:    map[string]string{"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InIyQGdtYWlsLmNvbSIsImlkIjoyMiwicm9sZSI6InJlYWRlciJ9.J3-92_iRT7sAJUBV3fxkHi_4kz3bCxNz6kDPBTSLeJQ"},
+			wantStatus: http.StatusNotFound,
+			wantKey:    "Message",
+			wantMsg:    "Book is not available you request has been denied",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/api/reader/raise-request/"+tt.query, nil)
+
+			for key, value := range tt.headers {
+				req.Header.Set(key, value)
+			}
+
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.wantStatus, w.Code)
+			var response map[string]interface{}
+			json.Unmarshal(w.Body.Bytes(), &response)
+
+			assert.Contains(t, response, tt.wantKey)
+			if tt.wantMsg != "" {
+				assert.Equal(t, tt.wantMsg, response[tt.wantKey])
+			}
+		})
+	}
+}
 
 func TestGetLibraries(t *testing.T) {
 	setupTestDB1()
